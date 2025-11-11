f7833b361be37c08ec6e20c6aeced182abe7a219
Kat-tech15 <kelvinkatwai@gmail.com>
2025-11-11 11:13:14 +0300

    modified main urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path 
 from .views import RegisterView, LoginView, LogoutView
+#from rest_framework_swagger.views import get_swagger_view
+#schema_view = get_swagger_view(title= 'SmartFarm API')
 
 
 urlpatterns = [
     path('', RegisterView.as_view(), name='register'),
+    #path('swagger_docs/', schema_view),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
