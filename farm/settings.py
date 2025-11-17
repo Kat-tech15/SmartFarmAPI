419b42b54460f18b8c06b8e48b637620352126ac
Katwai <kelvinkatwai@gmail.com>
2025-11-17 19:41:47 +0300

    modified settings

--- farm/settings.py
+++ farm/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
     'rest_framework',
     'drf_yasg',
     'accounts',
