fe8b983597d540815ec7b18ba4fac3386c3a4de8
Katwai <kelvinkatwai@gmail.com>
2025-11-19 17:19:07 +0300

    added otp verification

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import random
+from django.utils import timezone
+from datetime import timedelta
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
@@ -8,9 +11,30 @@ class CustomUser(AbstractUser):
     ]
     role = models.CharField(max_length=100, choices=ROLE_CHOICES, default='farmer')
     phone = models.CharField(max_length=10)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     location = models.CharField(max_length=100)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.username} ({self.role})"
     
+class EmailOTP(models.Model):
+    user =  models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expire_at = models.DateTimeField()
+    is_used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"OTP for {self.user.email} - {self.code}"
+    
+    @staticmethod
+    def generate_code():
+        return str(random.randint(100000, 999999))
+    
+    @staticmethod
+    def expiry_time():
+        return timezone.now() +timedelta(minutes=5)
+    
+    def is_expired(self):
+        return timezone.now() > self.expire_at
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,11 +3,11 @@ from accounts.models import CustomUser
 from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
@@ -17,6 +17,13 @@ class UserSerializer(serializers.ModelSerializer):
         )
         Token.objects.create(user=user)
         return user
+    
+class VerifyOTPSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    code = serializers.CharField(max_length=6)
+
+class ResendOTPSerializer(serializers.Serializer):
+    email = serializers.EmailField()
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
