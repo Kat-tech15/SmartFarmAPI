9ed7547dce5f0981e5812083978d161c6da063aa
Katwai <kelvinkatwai@gmail.com>
2025-11-19 17:55:02 +0300

    registered account models to the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ContactMessage, CustomUser, EmailOTP
 
-# Register your models here.
+admin.site.register(ContactMessage)
+admin.site.register(CustomUser)
+admin.site.register(EmailOTP)
