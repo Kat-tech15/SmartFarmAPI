764a51b75fcecbc171af9645c4f9283cd1f87b2f
Katwai <kelvinkatwai@gmail.com>
2025-11-20 17:25:14 +0300

    added allauth middleware

--- farm/settings.py
+++ farm/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.account.middleware.AccountMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
