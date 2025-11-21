1fe054653f04cfdc52b3245c05e12534f3128c34
Katwai <kelvinkatwai@gmail.com>
2025-11-21 12:58:46 +0300

    modified otp verification views

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,19 +3,27 @@ from . models import CustomUser, ContactMessage
 from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
+    otp_via = serializers.ChoiceField(
+        choices=[('email', 'Email'), ('sms', 'SMS')],
+        default='email',
+        write_only=True
+    )
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'phone', 'email', 'password', 'otp_via']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        otp_via = validated_data.pop('otp_via', 'email')
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            phone = validated_data['phone'],
             password=validated_data['password']
         )
         Token.objects.create(user=user)
+        user.otp_via = otp_via
         return user
     
 class VerifyOTPSerializer(serializers.Serializer):
