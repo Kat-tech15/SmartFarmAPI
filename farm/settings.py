8867e67b741fab44b221485bedc6ce01e1e58224
Kat-tech15 <kelvinkatwai@gmail.com>
2025-12-31 13:13:23 +0300

    modified settings.py

--- farm/settings.py
+++ farm/settings.py
@@ -58,9 +58,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
